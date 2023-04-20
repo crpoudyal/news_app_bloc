e8e2a0aec230d60412b270e3fe24e6f7a1c1fc24
crpoudyal <crpoudyal@gmail.com>
2023-04-20 16:35:20 +0545

    url updated

--- lib/utils/app_url.dart
+++ lib/utils/app_url.dart
@@ -2,5 +2,10 @@ class AppUrl {
   // ignore: non_constant_identifier_names
   static String ApiKey = "f6330cdff6244a53b8b7e0082020c8e2";
   static String newsUrl =
-      "https://newsapi.org/v2/top-headlines?country=in&apiKey=$ApiKey";
+      "https://newsapi.org/v2/top-headlines?country=in&category=sports&apiKey=$ApiKey";
+  static String businessNewsUrl =
+      "https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=$ApiKey";
+
+  static String techNewsUrl =
+      "https://newsapi.org/v2/top-headlines?country=in&category=technology&apiKey=$ApiKey";
 }
