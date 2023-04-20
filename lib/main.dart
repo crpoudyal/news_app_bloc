8a977118f67b7bebaf4c3cfb556d56774db7b2cc
crpoudyal <crpoudyal@gmail.com>
2023-04-20 16:40:38 +0545

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:news_app_bloc/bloc/business_bloc.dart';
 import 'package:news_app_bloc/bloc/news_bloc.dart';
+import 'package:news_app_bloc/bloc/tech_bloc.dart';
+import 'package:news_app_bloc/repositories/business_news_repo.dart';
 import 'package:news_app_bloc/repositories/news_repo.dart';
-
 import 'package:news_app_bloc/screens/splash_screen.dart';
+
+import 'repositories/tech_news_repo.dart';
 
 void main() {
   runApp(const MainApp());
@@ -19,6 +23,13 @@ class MainApp extends StatelessWidget {
         BlocProvider<NewsBloc>(
           create: (context) => NewsBloc(NewsRepo())..add(NewsLoadedEvent()),
         ),
+        BlocProvider<BusinessBloc>(
+          create: (context) =>
+              BusinessBloc(BusinessRepo())..add(BusinessLoadedEvent()),
+        ),
+        BlocProvider<TechBloc>(
+          create: (context) => TechBloc(TechRepo())..add(TechLoadedEvent()),
+        )
       ],
       child: const MaterialApp(
         debugShowCheckedModeBanner: false,
