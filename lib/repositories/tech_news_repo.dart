7fe8a823d9cd84af9abbb96e45be9ec16b0c557a
crpoudyal <crpoudyal@gmail.com>
2023-04-23 07:50:11 +0545

    tech news repo updated

---
+++ lib/repositories/tech_news_repo.dart
@@ -0,0 +1,31 @@
+import 'dart:convert';
+
+import 'dart:developer';
+
+import 'package:http/http.dart' as http;
+import 'package:news_app_bloc/models/news_model.dart';
+import 'package:news_app_bloc/utils/app_url.dart';
+
+class TechRepo {
+  Future<List<NewsModel>> getNews() async {
+    final dynamic response = await http.get(Uri.parse(AppUrl.techNewsUrl));
+
+    var data = jsonDecode(response.body);
+    List<NewsModel> newsModelList = [];
+    log('data : ${data['articles']}');
+
+    if (response.statusCode == 200) {
+      data['articles'].forEach((e) {
+        newsModelList.add(NewsModel.fromJson(e));
+      });
+
+      // log(" news List : ${newsModelList.forEach((element) { })}");
+      // for (var element in newsModelList) {
+      //   log("${element.title}");
+      // }
+      return newsModelList;
+    } else {
+      return newsModelList;
+    }
+  }
+}
